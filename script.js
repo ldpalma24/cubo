baf13cd7317382c507744bb09cb374441fab0666
Ruisuh <148378276+ldpalma24@users.noreply.github.com>
2023-11-11 11:28:46 -0400

    HOLA

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ var cubo = document.querySelector('.cubo');
 var isMouseDown = false;
 var lastMouseX, lastMouseY;
 var rotateX = 0, rotateY = 0;
+var timeoutId;
 
 // Cambia estos oyentes de eventos para que se activen en todo el documento
 document.addEventListener('mousedown', function(e) {
   isMouseDown = true;
   lastMouseX = e.clientX;
   lastMouseY = e.clientY;
+
+  // Inicia el temporizador cuando se presiona el botón del mouse
+  timeoutId = setTimeout(function() {
+    alert('AJAAAAA MI PANITA TODO BIEN');
+  }, 10000); // 10000 milisegundos son 10 segundos
 });
 
 document.addEventListener('mousemove', function(e) {
@@ -24,4 +30,7 @@ document.addEventListener('mousemove', function(e) {
 
 document.addEventListener('mouseup', function() {
   isMouseDown = false;
+
+  // Cancela el temporizador cuando se suelta el botón del mouse
+  clearTimeout(timeoutId);
 });
